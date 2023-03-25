88934bcdf8ac7adcc6e3c526f7444657aec2eef0
diestelkampw <bill_doh@yahoo.com>
2023-03-25 15:44:34 -0500

    initial upload working

--- orca_tweak_project/dell_tweak/views.py
+++ orca_tweak_project/dell_tweak/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
+
+
+def home(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'dell_tweak/home.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'dell_tweak/home.html')
